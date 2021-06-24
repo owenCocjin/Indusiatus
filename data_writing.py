ba9552f53864f3f724cd198bb8923e624dacf408
Owen Cocjin <owen_jamescf@hotmail.com>
2021-06-24 01:40:36 -0400

    Updated output format

--- data_writing.py
+++ data_writing.py
@@ -1,20 +1,25 @@
 ##
 ## Author:  Owen Cocjin
-## Version: 0.1
-## Date:    2021.06.20
+## Version: 0.2
+## Date:    2021.06.24
 ## Description:    Packet writing functions
-def write_1(packet, proto, fd):
-	'''Writes whole packet'''
-	fd.write(packet.getRaw())
-
-def write_2(packet, proto, fd):
-	'''Writes whole segment'''
-	fd.write(proto.getRaw())
-
-def write_3(packet, proto, fd):
-	'''Writes just the payload data'''
-	fd.write(proto.getPayload())
-
-write_funcs={'1':write_1,
-'2':write_2,
-'3':write_3}
+from ProgMenu.progmenu import MENU
+vprint=MENU.verboseSetup(['v', "verbose"])
+vname=__file__[__file__.rfind('/')+1:-3]
+def writeData(fd, data, layer=0):
+	'''Write raw data to fd.
+layer is the layer of data to write.
+If layer is larger than the size the data tuple, no data will be written'''
+	if layer<=len(data)-1:
+		towrite=data[layer].getRaw()
+		vprint(f"[|X:{vname}:writeData]: Writing {data[layer].getText().strip()}")
+		fd.write(b'\x3b')
+		fd.write(bytes.fromhex(f"{hex(len(towrite))[2:]:>04}"))
+		fd.write(b'\x3b')
+		fd.write(towrite)
+		fd.flush()
+		vprint(f"[|X:{vname}:writeData]: Success!")
+		return True
+	else:
+		vprint(f"[|X:{vname}:writeData]: Layer doesn't exist!")
+	return False
