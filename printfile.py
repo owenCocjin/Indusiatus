45e231d995f1975187390789a9f1d21356cfd844
Owen Cocjin <owen_jamescf@hotmail.com>
2021-07-03 17:16:44 -0400

    Small updates

--- printfile.py
+++ printfile.py
@@ -3,15 +3,23 @@
 ## Version: 0.1
 ## Date:    2021.06.24
 ## Description:    Print the bytes of the output file named "test.ind"
+## Notes:
+## Updates:
+##  - Added error for empty files
+##  - Prints packet count
 with open("test.ind", 'rb') as f:
 	menu=f.read(1)
+	if menu==b'':
+		print("[|X:printfile:main]: File empty!")
+		exit(0)
+	lines=0
 	while menu!=b'':
 		counter=0
 		#Get length
 		length=int(f.read(2).hex(),16)
 		menu=f.read(1)
 
-		print(f"Reading {length} bytes...")
+		print(f"Reading {length} bytes... (0x{hex(lines)[2:]:>04})")
 		for b in range(length):
 			menu=f.read(1)
 			print(f"0x{menu.hex()}", end=' ')
@@ -21,4 +29,6 @@ with open("test.ind", 'rb') as f:
 			elif counter%8==0:
 				print(' ', end='')
 		print('\n')
+		lines+=1
 		menu=f.read(1)
+	print(f"{lines} packets read!")
